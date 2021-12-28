32a509fe6c0bb173bb053a4e3d55c64439577dbf
anniebea <46000436+anniebea@users.noreply.github.com>
2021-12-28 12:08:54 +0200

    Bug fix: reported comment listing error.

--- database/migrations/2021_12_15_061653_create_comments_table.php
+++ database/migrations/2021_12_15_061653_create_comments_table.php
@@ -16,7 +16,7 @@ class CreateCommentsTable extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->string('content', 512);
-            $table->boolean('is_reported')->nullable();
+            $table->boolean('is_reviewed')->nullable();
             $table->foreignId('user_id')->constrained();
             $table->foreignId('sudoku_grid_id')->constrained();
             $table->timestamps();
