5b7f1978855ef49bbab3d54047777e54cb8beb81
anniebea <46000436+anniebea@users.noreply.github.com>
2022-01-09 17:05:01 +0200

    Bug fix: minor bug fixes

--- resources/views/admin/blockForm.blade.php
+++ resources/views/admin/blockForm.blade.php
@@ -13,20 +13,6 @@
                             @csrf
                             <input type="hidden" name="user_id" value="{{ $user->id }}">
 
-                            <div class="form-group row">
-                                <label for="date_to" class="col-md-4 col-form-label text-md-right">{{ __('Blocking end date') }}:</label>
-
-                                <div class="col-md-6">
-                                    <input id="date_to" type="date" class="form-control @error('date_to') is-invalid @enderror" name="date_to" value="{{ old('date_to') }}" autocomplete="date_to">
-
-                                    @error('date_to')
-                                    <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                    @enderror
-                                </div>
-                            </div>
-
                             <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-4">
                                     <button type="submit" class="btn btn-danger">
