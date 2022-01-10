4af9330271cd6528b453c73f7abe41d1db3e3484
anniebea <46000436+anniebea@users.noreply.github.com>
2022-01-10 10:36:28 +0200

    Bug fix: modal closing.

--- resources/views/sudoku/sudokuSolve.blade.php
+++ resources/views/sudoku/sudokuSolve.blade.php
@@ -132,7 +132,7 @@
                     <div class="modal-content">
                         <div class="modal-header">
                             <h2 class="modal-title">{{ __('Congratulations!') }}</h2>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
@@ -180,7 +180,7 @@
                             @endif
                         </div>
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                         </div>
                     </div>
                 </div>
@@ -192,7 +192,7 @@
                     <div class="modal-content">
                         <div class="modal-header">
                             <h5 class="modal-title">{{ __('Uh, Oh!') }}</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
@@ -200,7 +200,7 @@
                             <p> {{ __('Mistakes have been found!') }}</p>
                         </div>
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                         </div>
                     </div>
                 </div>
